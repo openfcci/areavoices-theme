adbdb4add8ac1f0836eb9e35ebe8852454c7767f
ryan.veitch <ryan.veitch@forumcomm.com>
2015-10-07 09:58:38 -0500

    Adblock header banner fix

--- template-parts/google_ads-header.php
+++ template-parts/google_ads-header.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- GOOGLE HEADER AD: Begin -->
-<div id="header-banner-ad" class="banner-ad">
+<div id="header-banner" class="banner">
 	 <!-- <img src="<?php // echo get_template_directory_uri(); ?>/images/avbannerad.png" role="advertising" alt="banner ad" /> -->  <!-- MAKE THIS AN AD SPACE -->
 	 <div id="fcc-areavoices-header-ad">
 		<div id="div-gpt-ad-1430258018861-0">
