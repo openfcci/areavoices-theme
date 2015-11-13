01f13fc8a8f7cf1046642f22e1421e4b9da7fd42
ryan.veitch <ryan.veitch@forumcomm.com>
2015-11-13 13:56:50 -0600

    Featured Slider post loop added

--- template-parts/featured_slider.php
+++ template-parts/featured_slider.php
@@ -5,57 +5,50 @@
  * @package areavoices
  */
 
+ // WP_Query arguments
+ $args = array (
+ 	'post_type'              => array( 'post' ),
+ 	'post_status'            => array( 'publish' ),
+ 	'posts_per_page'         => '6',
+ 	'ignore_sticky_posts'    => false,
+ );
+
+$loop = new WP_Query( $args );
+
+// Check that we have query results.
+if ( $loop->have_posts() ) { ?>
+  <!-- FEATURED SLIDER: Begin -->
+  <div id="responsive_check"></div>
+  <div id="slider">
+    <a class="control_next"><span class="avicon-arrow-long-right"></span></a>
+    <a class="control_prev"><span class="avicon-arrow-long-left"></span></a>
+    <ul>
+    <?php
+    while ( $loop->have_posts() ) {
+      $loop->the_post();
+      $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
+      $category = get_the_category();
+      ?>
+      <li>
+        <div class="featured-info-wrapper" style="background-image: url(<?php echo $thumbnail[0]; ?>); no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; height:100%">
+          <div class="featured-category"><?php echo esc_html( $category[0]->cat_name ) ?></div>
+          <div class="featured-title"><?php echo get_the_title(); ?></div>
+          <div class="featured-date"><?php echo get_post_time('F j, Y', true) ?></div>
+        </div>
+      </li>
+      <?php
+    }
+    ?>
+    </ul>
+  </div>
+  <!-- FEATURED SLIDER: End -->
+
+<?php }
+
 /*BS*/
 ?>
 
-<!-- FEATURED SLIDER: Begin -->
-<div id="responsive_check"></div>
-<div id="slider">
-  <a class="control_next"><span class="avicon-arrow-long-right"></span></a>
-  <a class="control_prev"><span class="avicon-arrow-long-left"></span></a>
-  <ul>
-    <li>
-      <div class="featured-info-wrapper">
-        <div class="featured-category">CATEGORY</div>
-        <div class="featured-title">POST TITLE 1</div>
-        <div class="featured-date">DD MMM YYYY</div>
-      </div>
-    </li>
-    <li style="background: #aaa;">
-      <div class="featured-info-wrapper">
-        <div class="featured-category">CATEGORY</div>
-        <div class="featured-title">POST TITLE 2</div>
-        <div class="featured-date">DD MMM YYYY</div>
-      </div>
-    </li>
-    <li>
-      <div class="featured-info-wrapper">
-        <div class="featured-category">CATEGORY</div>
-        <div class="featured-title">POST TITLE 3</div>
-        <div class="featured-date">DD MMM YYYY</div>
-      </div>
-    </li>
-    <li style="background: #aaa;">
-      <div class="featured-info-wrapper">
-        <div class="featured-category">CATEGORY</div>
-        <div class="featured-title">POST TITLE 4</div>
-        <div class="featured-date">DD MMM YYYY</div>
-      </div>
-    </li>
-    <li>
-      <div class="featured-info-wrapper">
-        <div class="featured-category">CATEGORY</div>
-        <div class="featured-title">POST TITLE 5</div>
-        <div class="featured-date">DD MMM YYYY</div>
-      </div>
-    </li>
-    <li style="background: #aaa;">
-      <div class="featured-info-wrapper">
-        <div class="featured-category">CATEGORY</div>
-        <div class="featured-title">POST TITLE 6</div>
-        <div class="featured-date">DD MMM YYYY</div>
-      </div>
-    </li>
-  </ul>  
-</div>
-<!-- FEATURED SLIDER: End -->
+
+
+
+<?php wp_reset_postdata(); // Restore original post data. ?>
